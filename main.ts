776ad9845d5c2ec4c883869759b5846c6c772a14
Tobias Lins <me@tobi.sh>
2021-08-05 16:01:18 +0200

    test sqlite

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
-import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
-import routes from "./routes.gen.ts";
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
-start(routes);
+const db = new DB("test.db");
+db.query(`
+  CREATE TABLE IF NOT EXISTS people (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+  )
+`);
+const names = ["Peter Parker", "Clark Kent", "Bruce Wayne"];
+
+// Run a simple query
+for (const name of names) {
+  db.query("INSERT INTO people (name) VALUES (?)", [name]);
+}
+
+addEventListener("fetch", async (event) => {
+  const res = await db.query("SELECT name FROM people");
+  const response = new Response("Hello World!" + res.toString(), {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
